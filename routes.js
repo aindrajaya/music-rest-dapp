27e204a34843d002de834b971d283680f354308a
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-11-25 08:52:00 +0700

    routes.js updated-v2
    add the routes for the register endpoint, this service will provide registration process for the user that want to access this music dapp serices

--- routes.js
+++ routes.js
@@ -1,7 +1,31 @@
 const shortid = require('shortid')
 function routes(app, db){
   //Register Endpoint
-  
+  app.post('/register', (req, res) => {
+    let email = req.body.email
+    let idd = shortid.generate()
+    if(email){
+      db.findOne({email}, (err, doc) => {
+        if(doc){
+          res.status(400).json({
+            "status": "Failed",
+            "reason": "Already registered"
+          })
+        } else {
+          db.insertOne({email})
+          res.json({
+            "status": "success",
+            "id": idd
+          })
+        }
+      })
+    } else {
+      res.status(400).json({
+        "status": "Failed",
+        "reason": "wrong input"
+      })
+    }
+  })
   //Login Endpoint
   
   //Upload Endpoint
