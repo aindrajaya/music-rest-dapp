39fa3dee20c88dbf1332e92bb4d43ddc86c857f6
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-11-25 08:44:00 +0700

    routes.js updated-v1
    write default code structure for server.js that will navigate to each endpoint which is used in this backend servive

---
+++ routes.js
@@ -0,0 +1,12 @@
+const shortid = require('shortid')
+function routes(app, db){
+  //Register Endpoint
+  
+  //Login Endpoint
+  
+  //Upload Endpoint
+
+  //Access endpoint, for specific music id
+
+  //Access endpoint, for entire library
+}
