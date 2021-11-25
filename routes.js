81d7949ec8124d90d79e90542739a53bf290306c
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-11-25 08:59:00 +0700

    routes.js updated-v3
    add the login endpoint, this endpoint focuses on how the user login to the music dapp service by using an email

--- routes.js
+++ routes.js
@@ -26,7 +26,31 @@ function routes(app, db){
       })
     }
   })
+
   //Login Endpoint
+  app.post('/login', (req, res) => {
+    let email = req.body.email
+    if(email){
+      db.findOne({email}, (err, doc) => {
+        if(doc){
+          res.json({
+            "status":"success",
+            "id": doc.id
+          })
+        } else {
+          res.satus(400).json({
+            "status": "Failed",
+            "reason": "Not recognised"
+          })
+        }
+      })
+    } else {
+      res.status(400).json({
+        "status": "Failed",
+        "reason":"wrong input"
+      })
+    }
+  })
   
   //Upload Endpoint
 
