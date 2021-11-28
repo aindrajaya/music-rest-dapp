e1ad57fc8ae021ad5e9bcecf4ab9c9635fd2f85a
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-11-28 10:11:00 +0700

    updated contract v3
    add the event for the smart contract, we use two events that will sent the ipfs hash address and stored response

--- contracts/StoredMusic.sol
+++ contracts/StoredMusic.sol
@@ -4,4 +4,8 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Stored {
  //Structure
  mapping(string => string) public ipfsStored;
+
+ //Events
+ event ipfsSent(string _ipfsHash, string _address);
+ event storedResponse(string response);
 }
