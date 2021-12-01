530673f13b722c05ff4a575168ac58d3b656e46d
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-12-02 04:38:00 +0700

    updated the test-v2
    test to call and wait for the deployed process of the contract

--- test/StoredMusicTest.js
+++ test/StoredMusicTest.js
@@ -3,8 +3,9 @@ const IPFSStored = artifacts("./contracts/StoredMusic.sol")
 contract("IPFSStored test", accounts => {
   //declatre the test
   it("emit event when you send a ipfs address", async () => {
-    await console.log(accounts)
     //1. Call and wait for the contract
+    const ipfsStored = await IPFSStored.deployed()
+
     //2. Set the variable to false and get event listener
     //3. Call the contract function which sends ipfs address
   })
