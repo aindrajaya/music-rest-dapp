d0c0617ea25ceb86192af0dcd598085446eedca0
Arista Indrajaya <arista.indrajaya@gmail.com>
2021-12-02 04:34:00 +0700

    updated the test-v1
    add the test structure to know what will to write in this test file

--- test/StoredMusicTest.js
+++ test/StoredMusicTest.js
@@ -1,5 +1,11 @@
 const IPFSStored = artifacts("./contracts/StoredMusic.sol")
 
 contract("IPFSStored test", accounts => {
-  //do the test
+  //declatre the test
+  it("emit event when you send a ipfs address", async () => {
+    await console.log(accounts)
+    //1. Call and wait for the contract
+    //2. Set the variable to false and get event listener
+    //3. Call the contract function which sends ipfs address
+  })
 })
